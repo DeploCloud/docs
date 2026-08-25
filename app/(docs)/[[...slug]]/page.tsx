e7e520dc30591b73c8ae9dc939e8411238deb4a1
IdraDev <dragonfx1234@gmail.com>
2026-08-25 12:53:17 +0200

    feat: add nested API reference docs, edit-on-GitHub links, and favicon
    - Add API Reference section (Deployments, Projects > Webhooks, Domains, Errors)
      as a collapsible nested sidebar tree
    - Wire fumadocs' editOnGithub button on every page, pointing at DeploCloud/docs
    - Add favicon/apple-icon/icon assets matching deplo-web, drop unused icon.svg

--- app/(docs)/[[...slug]]/page.tsx
+++ app/(docs)/[[...slug]]/page.tsx
@@ -12,7 +12,16 @@ export default async function Page(props: PageProps<"/[[...slug]]">) {
   const MDXContent = page.data.body;
 
   return (
-    <DocsPage toc={page.data.toc} full={page.data.full}>
+    <DocsPage
+      toc={page.data.toc}
+      full={page.data.full}
+      editOnGithub={{
+        owner: "DeploCloud",
+        repo: "docs",
+        sha: "main",
+        path: `content/docs/${page.path}`,
+      }}
+    >
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription>{page.data.description}</DocsDescription>
       <DocsBody>
